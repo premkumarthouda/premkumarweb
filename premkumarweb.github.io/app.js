24bde8f7566a3a8a49c22777471392e6aa5f75f1
premkumarthouda <113364219+premkumarthouda@users.noreply.github.com>
2024-09-15 22:25:27 +0530

    Update app.js

--- premkumarweb.github.io/app.js
+++ premkumarweb.github.io/app.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
-    for (const image of images){
+    for (const image of images) {
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json())
-        .then(data=> {
-            image.src=data.message
+        .then(data => {
+            image.src = data.message;
             image.width = 100;
-            image.height=100;
+            image.height = 100;
         })
+        .catch(error => {
+            console.error("Error fetching dog image:", error);
+            image.alt = "Failed to load image"; // Sets alt text if fetching fails
+        });
     }
-})
+});
+
